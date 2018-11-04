f6985f56477f59c783b812162840dee89648f0ed
nikolaytsvetin <nikolaytsvetin@gmail.com>
2018-11-04 11:40:26 +0200

    Binary search tree and Linked list data structures

---
+++ Binary Search Tree/BinarySearchTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node bst = new Node(10);
+
+            bst.insert(5);
+            bst.insert(15);
+            bst.insert(38);
+            bst.insert(3);
+            bst.insert(14);
+
+            Console.WriteLine("IN ORDER TRAVERSAL");
+            bst.inOrderTraversal();
+            Console.WriteLine("PRE ORDER TRAVERSAL");
+            bst.preOrderTraversal();
+            Console.WriteLine("POST ORDER TRAVERSAL");
+            bst.postOrderTraversal();
+        }
+    }
+}
---
+++ Linked List/LinkedList/LinkedList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<T>
+    {
+        private Node<T> head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public void AddLast(T newValue)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(newValue);
+                head.Next = null;
+            }
+            else
+            {
+                Node<T> currentElement = head;
+
+                while (currentElement.Next != null)
+                {
+                    currentElement = currentElement.Next;
+                }
+
+                currentElement.Next = new Node<T>(newValue);
+            }
+        }
+
+        public void AddFirst(T newValue)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(newValue);
+                head.Next = null;
+            }
+            else
+            {
+                Node<T> toAdd = new Node<T>(newValue);
+                Node<T> previousHead = head;
+                head = toAdd;
+                head.Next = previousHead;
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            if (head == null)
+            {
+                return;
+            }
+            else
+            {
+                //Node<T> elementToRemove = head;
+                //Node<T> newHead = head.Next;
+
+                head = head.Next;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (head == null)
+            {
+                return;
+            }
+            else
+            {
+                Node<T> currentELement = head;
+                Node<T> previousElement = head;
+
+                while (currentELement.Next != null)
+                {
+                    previousElement = currentELement;
+                    currentELement = currentELement.Next;
+                }
+
+                previousElement.Next = null;
+            }
+        }
+
+        public void PrintAllNodes()
+        {
+            Node<T> currentElement = head;
+            StringBuilder result = new StringBuilder();
+
+            while (currentElement != null)
+            {
+                result.Append($" {currentElement.Value}");
+              //  Console.WriteLine(result.ToString());
+                currentElement = currentElement.Next;
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
