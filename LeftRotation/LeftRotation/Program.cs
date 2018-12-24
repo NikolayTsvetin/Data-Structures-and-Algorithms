e92d7a47878e68ac4afc89fc763cc3aa23323534
nikolaytsvetin <nikolaytsvetin@gmail.com>
2018-12-24 09:59:57 +0200

    Left rotation of array

---
+++ LeftRotation/LeftRotation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            int timesToShift = 3;
+            int[] shiftedArray = leftRotate(array, array.Length, timesToShift);
+
+            Console.WriteLine(string.Join(", ", shiftedArray));
+        }
+
+        static int[] leftRotate(int[] array, int length, int shift)
+        {
+            int[] copy = new int[length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentEl = array[i];
+                int indexToBePlaced = i - shift;
+
+                while (indexToBePlaced < 0)
+                {
+                    indexToBePlaced = length + indexToBePlaced;
+                }
+
+                copy[indexToBePlaced] = currentEl;
+            }
+
+            return copy;
+        }
+    }
+}
---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> unsortedNumbers = new List<int>() { 14, 13, 56, 123, 52, 6, 12, 0 };
+
+            List<int> sortedNumbers = Quicksort(unsortedNumbers, 0, unsortedNumbers.Count - 1);
+
+            Console.WriteLine(string.Join(", ", sortedNumbers));
+        }
+
+        private static List<int> Quicksort(List<int> unsortedNumbers, int startIndex, int endIndex)
+        {
+            int pivot = unsortedNumbers[0];
+            List<int> smaller = new List<int>();
+            List<int> bigger = new List<int>();
+
+            for (int i = 1; i < unsortedNumbers.Count; i++)
+            {
+                if (unsortedNumbers[i] < pivot)
+                {
+                    smaller.Add(unsortedNumbers[i]);
+                }
+                else
+                {
+                    bigger.Add(unsortedNumbers[i]);
+                }
+            }
+
+            smaller.Add(pivot);
+
+            var result = smaller.Concat(bigger).ToList();
+
+            return result;
+        }
+    }
+}
