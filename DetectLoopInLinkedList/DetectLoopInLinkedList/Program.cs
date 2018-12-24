8fefb8f1eaa84fd8e5d913b1ec3d42cf5d6fcafc
nikolaytsvetin <nikolaytsvetin@gmail.com>
2018-12-24 12:29:38 +0200

    Detect loop in singly linked list

---
+++ DetectLoopInLinkedList/DetectLoopInLinkedList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectLoopInLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static bool hasCycle(SinglyLinkedListNode head)
+        {
+            var currentEl = head;
+            Dictionary<SinglyLinkedListNode, int> dict = new Dictionary<SinglyLinkedListNode, int>();
+
+            if (head == null)
+            {
+                return false;
+            }
+
+            while (currentEl.Next != null)
+            {
+                if (!dict.ContainsKey(currentEl))
+                {
+                    dict.Add(currentEl, currentEl.Data);
+                }
+                else
+                {
+                    return true;
+                }
+
+                currentEl = currentEl.Next;
+            }
+
+            return false;
+        }
+    }
+}
