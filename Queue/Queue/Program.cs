ed282f74f924021f553c985c2aea3458ff2a4ca3
nikolaytsvetin <nikolaytsvetin@gmail.com>
2019-01-30 23:06:59 +0200

    Queue<T> implementation

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<int> queue = new MyQueue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            Console.WriteLine(queue.ToString());
+
+            queue.Dequeue();
+            queue.Dequeue();
+            Console.WriteLine(queue.ToString());
+
+            queue.Clean();
+            Console.WriteLine(queue.ToString());
+        }
+    }
+}
